d6b28b3ffba8ac0e36ad2ce941f83aed7477e2a3
ssm-08 <shreesainarayanm@gmail.com>
2026-02-20 12:34:46 -0800

    initialized functions and program loop

--- Heap.cpp
+++ Heap.cpp
@@ -3,9 +3,38 @@
 
 using namespace std;
 
+int add(int n, int h[100]);
+int remove(int h[100]);
+int print(int h[100]);
+
 int main() {
 
-  // Heap
+  int heap[100] = {};
 
+  bool run = true;
   
+  char input[99] = "";
+  
+  const char* ADD = "ADD";
+  const char* PRINT = "PRINT";
+  const char* REM = "REMOVE";
+  const char* QUIT = "QUIT";
+  
+  while (run == true) {
+
+    // Get input
+    cout << "\n";
+    cout << "Enter Command: ";
+    cin >> input;
+    cout << "\n";
+
+    int n = 0;
+    
+    if (strcmp(input, ADD) == 0) { // Add num
+    } else if (strcmp(input, PRINT) == 0) { // Print heap
+    } else if (strcmp(input, REM) == 0) { // Remove root
+    } else if (strcmp(input, QUIT) == 0) {
+      run = false;
+    }
+
 }
